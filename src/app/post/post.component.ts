bcab8d5cd6b7e6fdf6aae1c6fe5a4c666abe4eff
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2023-04-20 12:13:09 +0530

    made a post component

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,66 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+} from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
-export class PostComponent {
-  @Input() postImg = ''
+export class PostComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
+  @Input() postImg = '';
   @Output() imgSelected = new EventEmitter<string>();
+
+  constructor() {
+    // occurs before binding
+    console.log('constructor() called', this.postImg);
+  }
+
+  ngOnInit() {
+    // occurs after binding has been initialized
+    console.log('ngOnInit() called', this.postImg);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck() called');
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges() called');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit() called');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked() called');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit() called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked() called');
+  }
 }
